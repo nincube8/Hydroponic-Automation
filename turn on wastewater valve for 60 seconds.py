37c2009ac264e28c7a2a7a0f55526266cdbf8a6b
CKose <cevatkose@gmail.com>
2021-01-12 18:37:21 -0700

    Update turn on wastewater valve for 60 seconds.py

--- turn on wastewater valve for 60 seconds.py
+++ turn on wastewater valve for 60 seconds.py
@@ -10,7 +10,7 @@ for i in pinList:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 # time to sleep between operations in the main loop
-    SleepTimeL = 1200
+    SleepTimeL = 1200 #20 minutes
 # main loop
 try:
 #"Pin #"
